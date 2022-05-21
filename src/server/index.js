6d0177b98dcf9860f4df58bf6483171f9bc36a7d
josep andreu <josepa45@gmail.com>
2022-05-21 12:12:14 +0200

    Created error middlewares

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const { error404, generalError } = require("./middlewares/errors");
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.use(error404);
+app.use(generalError);
 
 module.exports = app;
