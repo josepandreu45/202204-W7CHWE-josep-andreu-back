b201026009fedf1c4872551fbba86b44f78f7cd4
josep andreu <josepa45@gmail.com>
2022-05-21 12:04:36 +0200

    Conocted to database

---
+++ src/database/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const debug = require("debug")("socialnetwork:database");
+const mongoose = require("mongoose");
+
+const conectDB = (dbUrl) =>
+  new Promise((resolve, reject) => {
+    mongoose.set("debug", true);
+    mongoose.connect(dbUrl, (error) => {
+      if (error) {
+        debug("conecting error to database", error.message);
+        reject();
+        return;
+      }
+      debug("conected to database");
+      resolve();
+    });
+  });
+module.exports = conectDB;
