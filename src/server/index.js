7b64ed7333b98b273fcca7d2ed5076752771efde
josep andreu <josepa45@gmail.com>
2022-05-22 16:28:46 +0200

    Created userRegister controller

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
 const { error404, generalError } = require("./middlewares/errors");
 const usersRouters = require("./routers/usersRouters");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
-
+app.use(helmet());
+app.use(express.static("uploads"));
 app.use(express.json());
 
 app.use("/users", usersRouters);
